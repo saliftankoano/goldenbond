5d58942dbd420b5afaf3196d803296e35ba14979
salif <tanksalif@gmail.com>
2025-05-21 22:44:44 -0400

    add footer basic

---
+++ app/(landing)/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#1B0E01] p-4">
+      <p className="text-center text-white">© 2024 Your Company</p>
+    </footer>
+  );
+}
