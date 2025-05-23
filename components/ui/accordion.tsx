77b5eb6a0747dc8fa5929f50081b9e053a914cdf
salif <tanksalif@gmail.com>
2025-05-22 22:23:29 -0400

    adjust accordion style to brand

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import * as AccordionPrimitive from "@radix-ui/react-accordion"
-import { Plus } from "lucide-react"
+import { Plus, X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
@@ -19,7 +19,7 @@ function AccordionItem({
   return (
     <AccordionPrimitive.Item
       data-slot="accordion-item"
-      className={cn("border-b last:border-b-0", className)}
+      className={cn("border-b border-[#B58E5A]/37 last:border-b-0", className)}
       {...props}
     />
   )
@@ -35,13 +35,16 @@ function AccordionTrigger({
       <AccordionPrimitive.Trigger
         data-slot="accordion-trigger"
         className={cn(
-          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
+          "group focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50",
           className
         )}
         {...props}
       >
         {children}
-        <Plus className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
+        <div className="pointer-events-none size-4 shrink-0 translate-y-0.5 relative">
+          <Plus className="text-[#B58E5A] transition-transform duration-200 absolute group-data-[state=open]:scale-0" />
+          <X className="text-[#757575] transition-all duration-500 absolute scale-0 group-data-[state=open]:scale-100 group-data-[state=open]:rotate-180" />
+        </div>
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
   )
