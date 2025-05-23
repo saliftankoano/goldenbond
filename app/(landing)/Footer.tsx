6c23b25e7a4f9030841934314cb056a8c8aef7e3
salif <tanksalif@gmail.com>
2025-05-22 22:33:24 -0400

    adjust footer responsiveness

--- app/(landing)/Footer.tsx
+++ app/(landing)/Footer.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#1B0E01] p-4 tenor-font px-[115px] py-[120px] w-full max-h-[468px]">
-        <div className="footer-core max-w-[1210px] flex justify-between">
-            <div className="footer-core-left">
+    <footer className="bg-[#1B0E01] p-4 tenor-font px-6 sm:px-12 md:px-20 lg:px-[115px] py-12 sm:py-16 md:py-20 lg:py-[120px] w-full">
+        <div className="footer-core max-w-[1210px] mx-auto flex flex-col lg:flex-row justify-between items-center lg:items-start text-center lg:text-left">
+            <div className="footer-core-left flex flex-col items-center lg:items-start">
                 <div className="footer-brand">
                     <Image src="/images/logo.svg" alt="logo" width={117} height={117} />
                 </div>
@@ -20,10 +20,10 @@ export default function Footer() {
                     <Image src="/images/tt.svg" alt="socials" width={24} height={24} />
                 </div>
             </div>
-            <div className="footer-core-right flex gap-[44px]">
+            <div className="footer-core-right flex flex-col sm:flex-row gap-8 sm:gap-10 md:gap-[44px] mt-10 lg:mt-0">
                 <div className="links-1">
                     <Link href="/education" >
-                        <p className="text-[#FBD6A4] pb-[38px]" >Education</p>
+                        <p className="text-[#FBD6A4] pb-4 sm:pb-[38px]" >Education</p>
                     </Link>
                     <Link href="/portfolio" >
                         <p className="text-[#B58E5A]">Portfolio</p>
@@ -31,27 +31,27 @@ export default function Footer() {
                 </div>
                 <div className="links-2">
                     <Link href="/events" >
-                        <p className="text-[#FBD6A4] pb-[38px]">Events</p>
+                        <p className="text-[#FBD6A4] pb-4 sm:pb-[38px]">Events</p>
                     </Link>
                     <Link href="/private-parties" >
-                        <p className="text-[#B58E5A] pb-[12px]">Private Parties</p>
+                        <p className="text-[#B58E5A] pb-2 sm:pb-[12px]">Private Parties</p>
                     </Link>
                     <Link href="/corporate-events" >
-                        <p className="text-[#B58E5A] pb-[12px]">Corporate Events</p>
+                        <p className="text-[#B58E5A] pb-2 sm:pb-[12px]">Corporate Events</p>
                     </Link>
                     <Link href="/pop-up-events" >
-                        <p className="text-[#B58E5A] pb-[12px]">Pop-Up</p>
+                        <p className="text-[#B58E5A] pb-2 sm:pb-[12px]">Pop-Up</p>
                     </Link>
                 </div>
                 <div className="links-3">
                     <Link href="/resources" >
-                        <p className="text-[#FBD6A4] pb-[38px]">Resources</p>
+                        <p className="text-[#FBD6A4] pb-4 sm:pb-[38px]">Resources</p>
                     </Link>    
                     <Link href="/home" >
-                        <p className="text-[#B58E5A] pb-[12px]">Home</p>
+                        <p className="text-[#B58E5A] pb-2 sm:pb-[12px]">Home</p>
                     </Link>
                     <Link href="/story-time" >
-                        <p className="text-[#B58E5A] pb-[12px]">Story Time</p>
+                        <p className="text-[#B58E5A] pb-2 sm:pb-[12px]">Story Time</p>
                     </Link>
                     <Link href="/about" >
                         <p className="text-[#B58E5A]">About</p>
@@ -59,7 +59,7 @@ export default function Footer() {
                 </div>
             </div>
         </div>
-        <div className="footer-bottom mt-[70px]">
+        <div className="footer-bottom mt-10 sm:mt-12 md:mt-[70px]">
             <p className="text-center text-[#FBD6A4]">Copyright © 2025, Golden Bond. All rights reserved. </p>
         </div>
     </footer>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="w-full h-full">
       <Faq />
-     <Footer />
+      <Footer />
     </div>
   );
 }
