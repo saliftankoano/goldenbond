af58fb766749ea9eb56ca511a241140423f7c38b
salif <tanksalif@gmail.com>
2025-05-25 14:45:30 -0400

    add testimonials basic

---
+++ app/(landing)/Testimonials.tsx
@@ -0,0 +1,10 @@
+"use client"
+export default function Testimonials() {
+    return (
+        <section className="w-full h-full bg-[#F4F1EB]">
+            <div className="w-full h-full bg-[#F4F1EB]">
+                <h1>Testimonials</h1>
+            </div>
+        </section>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import Footer from "./(landing)/Footer";
 import Faq from "./(landing)/Faq";
 import Who from "./(landing)/Who";
 import Cta from "./(landing)/Cta";
+import Testimonials from "./(landing)/Testimonials";
 export default function Home() {
   return (
     <div className="w-full h-full bg-[#F4F1EB]">
+      <Testimonials />
       <Cta />
       <Who />
       <Faq />
