b653a771c7a1a23a6dc3eb4f3f9c289130b15607
salif <tanksalif@gmail.com>
2025-06-03 17:25:13 -0400

    add events to story-time

--- app/story-time/page.tsx
+++ app/story-time/page.tsx
@@ -3,14 +3,14 @@
 import Header from "@/app/components/StaticHeader";
 import Hero from "./components/Hero";
 import Footer from "@/app/components/Footer";
-import Categories from "@/app/categories/page";
+import Events from "@/app/story-time/components/Events";
 
 export default function StoryTime() {
   return (
     <>
       <Header />
       <Hero />
-      <Categories />
+      <Events />
       <Footer />
     </>
   );
