df6fb864ca823aa5d500ad829dfaa3dc10399104
salif <tanksalif@gmail.com>
2025-06-03 18:26:52 -0400

    add testimonials

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -5,12 +5,14 @@ import Footer from "@/app/components/Footer";
 import PortfolioHero from "./components/Hero";
 import Faq from "@/app/components/Faq";
 import Cta from "../components/Cta";
+import Testimonials from "../components/Testimonials";
 
 export default function Portfolio() {
   return (
     <>
       <Header />
       <PortfolioHero />
+      <Testimonials />
       <Cta />
       <Faq />
       <Footer />
