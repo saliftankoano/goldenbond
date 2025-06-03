7bcc675a3588bfbed05d5af22bfd274acdafe68f
salif <tanksalif@gmail.com>
2025-06-03 18:21:07 -0400

    add story-time article

---
+++ app/story-time/article/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Footer from "@/app/components/Footer";
+import Header from "@/app/components/StaticHeader";
+import Cta from "@/app/components/Cta";
+import Hero from "./components/Hero";
+
+export default function StoryTimeArticle() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Cta />
+      <Footer />
+    </>
+  );
+}
