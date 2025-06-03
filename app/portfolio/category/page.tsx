ec326d991412027ca19d853859f3b85eb5ebb406
salif <tanksalif@gmail.com>
2025-06-03 19:23:58 -0400

    add basic portfolio category

---
+++ app/portfolio/category/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Header from "@/app/components/StaticHeader";
+import Footer from "@/app/components/Footer";
+import Testimonials from "@/app/components/Testimonials";
+import Cta from "@/app/components/Cta";
+import Faq from "@/app/components/Faq";
+
+export default function PortfolioCategory() {
+  return (
+    <>
+      <Header />
+      <Testimonials />
+      <Cta />
+      <Faq />
+      <Footer />
+    </>
+  );
+}
