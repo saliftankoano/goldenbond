4c5987b6eb1122a4e6fe3350baf0a987779228ba
salif <tanksalif@gmail.com>
2025-06-03 18:26:34 -0400

    add cta to portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -4,12 +4,14 @@ import Header from "@/app/components/StaticHeader";
 import Footer from "@/app/components/Footer";
 import PortfolioHero from "./components/Hero";
 import Faq from "@/app/components/Faq";
+import Cta from "../components/Cta";
 
 export default function Portfolio() {
   return (
     <>
       <Header />
       <PortfolioHero />
+      <Cta />
       <Faq />
       <Footer />
     </>
