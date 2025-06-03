c1e109ead0431b4faa4a29d1638f8c26fe393778
salif <tanksalif@gmail.com>
2025-06-03 17:36:46 -0400

    add faq to story-time

--- app/story-time/page.tsx
+++ app/story-time/page.tsx
@@ -6,6 +6,7 @@ import Footer from "@/app/components/Footer";
 import Events from "@/app/story-time/components/Events";
 import Testimonials from "@/app/components/Testimonials";
 import Cta from "@/app/components/Cta";
+import Faq from "@/app/components/Faq";
 
 export default function StoryTime() {
   return (
@@ -15,6 +16,7 @@ export default function StoryTime() {
       <Events />
       <Testimonials />
       <Cta />
+      <Faq />
       <Footer />
     </>
   );
