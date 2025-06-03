1cce4a0a9553d9a0b3da5060190c430ab1edd010
salif <tanksalif@gmail.com>
2025-06-03 18:25:32 -0400

    add portfolio page

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Header from "@/app/components/StaticHeader";
+import Footer from "@/app/components/Footer";
+import PortfolioHero from "./components/Hero";
+
+export default function Portfolio() {
+  return (
+    <>
+      <Header />
+      <PortfolioHero />
+      <Footer />
+    </>
+  );
+}
