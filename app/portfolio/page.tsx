e5177c5178ac848011947b3df8d71c2c9aca9c31
salif <tanksalif@gmail.com>
2025-06-03 18:26:13 -0400

    add faq to portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -3,12 +3,14 @@
 import Header from "@/app/components/StaticHeader";
 import Footer from "@/app/components/Footer";
 import PortfolioHero from "./components/Hero";
+import Faq from "@/app/components/Faq";
 
 export default function Portfolio() {
   return (
     <>
       <Header />
       <PortfolioHero />
+      <Faq />
       <Footer />
     </>
   );
