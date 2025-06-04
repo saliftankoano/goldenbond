74361bdb34fb555445f8b1fe01905e7ff140ab15
salif <tanksalif@gmail.com>
2025-06-03 22:48:02 -0400

    add content to story-time article

--- app/story-time/article/page.tsx
+++ app/story-time/article/page.tsx
@@ -4,12 +4,14 @@ import Footer from "@/app/components/Footer";
 import Header from "@/app/components/StaticHeader";
 import Cta from "@/app/components/Cta";
 import Hero from "./components/Hero";
+import Content from "./components/Content";
 
 export default function StoryTimeArticle() {
   return (
     <>
       <Header />
       <Hero />
+      <Content />
       <Cta />
       <Footer />
     </>
