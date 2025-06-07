f32073e2b49bafcba78edb1807f5e574f10b576a
salif <tanksalif@gmail.com>
2025-06-07 17:36:50 -0400

    add basic private party components

--- app/private-party/page.tsx
+++ app/private-party/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import StaticHeader from "@/app/components/StaticHeader";
+import Footer from "@/app/components/Footer";
+import Faq from "@/app/components/Faq";
 
 export default function PrivateParty() {
   return (
     <>
       <StaticHeader />
+      <Faq />
+      <Footer />
     </>
   );
 }
