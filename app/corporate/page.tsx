dd4d648e63a0a1b54b03edef3ab14045a1f97370
salif <tanksalif@gmail.com>
2025-06-07 17:36:27 -0400

    add basic corporate page structure

---
+++ app/corporate/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import StaticHeader from "@/app/components/StaticHeader";
+import Faq from "@/app/components/Faq";
+import Footer from "@/app/components/Footer";
+
+export default function Corporate() {
+  return (
+    <>
+      <StaticHeader />
+      <Faq />
+      <Footer />
+    </>
+  );
+}
