c4ac925191ade46e77330ef83d9098819032321d
salif <tanksalif@gmail.com>
2025-06-08 18:15:56 -0400

    modify comment for commit test

--- app/private-party/components/Hero.tsx
+++ app/private-party/components/Hero.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-// Test precommit hook test
+// Test precommit hook tes
 export default function Hero() {
   return (
     <section className="private-party-hero flex bg-[#F4F1EB] relative">
