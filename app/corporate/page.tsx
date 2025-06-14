f588779d2579b4667d2bcdcd20af7da57e1bbbca
salif <tanksalif@gmail.com>
2025-06-13 23:49:43 -0400

    update corporate

--- app/corporate/page.tsx
+++ app/corporate/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import StaticHeader from "@/app/components/StaticHeader";
-import Faq from "@/app/components/Faq";
 import Footer from "@/app/components/Footer";
+import Faq from "@/app/components/Faq";
+import Hero from "@/app/corporate/components/Hero";
+import Find from "@/app/corporate/components/Find";
+import PastEvents from "@/app/corporate/components/PastEvents";
 
-export default function Corporate() {
+export default function PrivateParty() {
   return (
     <>
       <StaticHeader />
+      <Hero />
+      <Find />
+      <PastEvents />
       <Faq />
       <Footer />
     </>
